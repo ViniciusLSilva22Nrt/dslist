085b23a7d36701e30cbdfb3849b3c1d1c72d820b
Vinícius Lacerda <viniciusdw123@gmail.com>
2025-01-21 20:56:46 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,8 +22,13 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platform;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -31,7 +36,7 @@ public class Game {
 	}
 
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 
 		this.id = id;
@@ -39,6 +44,7 @@ public class Game {
 		this.year = year;
 		this.genre = genre;
 		this.platform = platform;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -92,6 +98,17 @@ public class Game {
 
 	public void setPlatform(String platform) {
 		this.platform = platform;
+	}
+
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
